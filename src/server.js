871c911ff3c95bc6c40485eec18704c273e30c4b
Krish Soni <sonikrish994@gmail.com>
2024-09-05 00:07:51 +0530

    waitlist init

--- src/server.js
+++ src/server.js
@@ -11,9 +11,18 @@ app.use(cors(
 ));
 connectToDB();
 
+
+app.get('/', (req, res) => {
+    res.send('Welcome to SAM API');
+
+});
 app.post('/api/waitlist', async (req, res) => {
     const { name, email } = req.body;
     try {
+        const existingEmail = await waitlist.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).send('Email already exists in waitlist');
+        }
         await waitlist.create({ name, email });
         res.status(201).send('Added to waitlist');
     } catch (error) {
